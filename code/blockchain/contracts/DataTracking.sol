e2d80b29bf218d2ce376621c4dd7ba9a74eae94b
Abrar Ahmed <abrarahmedny@gmail.com>
2026-02-28 10:43:51 +0500

    Implement updates for better performance

--- code/blockchain/contracts/DataTracking.sol
+++ code/blockchain/contracts/DataTracking.sol
@@ -2,28 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract DataTracking {
-    struct MarketData {
-        uint256 timestamp;
-        uint256 price;
-        uint256 volume;
-    }
+  struct MarketData {
+    uint256 timestamp;
+    uint256 price;
+    uint256 volume;
+  }
 
-    mapping(string => MarketData[]) public assetData; // Ticker => Data
-    address public owner;
+  mapping(string => MarketData[]) public assetData; // Ticker => Data
+  address public owner;
 
-    constructor() {
-        owner = msg.sender;
-    }
+  constructor() {
+    owner = msg.sender;
+  }
 
-    event NewData(string indexed ticker, uint256 price, uint256 volume);
+  event NewData(string indexed ticker, uint256 price, uint256 volume);
 
-    function addDataPoint(string memory ticker, uint256 price, uint256 volume) external {
-        require(msg.sender == owner, 'Unauthorized');
-        assetData[ticker].push(MarketData(block.timestamp, price, volume));
-        emit NewData(ticker, price, volume);
-    }
+  function addDataPoint(string memory ticker, uint256 price, uint256 volume) external {
+    require(msg.sender == owner, 'Unauthorized');
+    assetData[ticker].push(MarketData(block.timestamp, price, volume));
+    emit NewData(ticker, price, volume);
+  }
 
-    function getHistoricalData(string memory ticker) external view returns (MarketData[] memory) {
-        return assetData[ticker];
-    }
+  function getHistoricalData(string memory ticker) external view returns (MarketData[] memory) {
+    return assetData[ticker];
+  }
 }
